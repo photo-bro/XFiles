aaf368829fb36d1370f702c01bbec7ad57eff607
photo-bro <photigragraphy@gmail.com>
2013-11-21 11:26:05 -0600

    Query GUI for user

---
+++ Forms/UserQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XFiles.Forms
+{
+    public partial class UserQuery : Form
+    {
+        public UserQuery()
+        {
+            InitializeComponent();
+        }
+
+        private void UserQuery_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}

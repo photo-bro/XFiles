e3599f042105f7681c9f6f4a7dec9ad697aa0bb9
unknown <josh.harmon@ED-001969.principia.local>
2013-11-20 21:17:29 -0600

    Login() form is new main point of entry

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using XFiles.Forms; // Login
 
 namespace XFiles
 {
@@ -16,6 +17,7 @@ namespace XFiles
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
             Application.Run(new Database());
         }
     }

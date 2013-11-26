798c42f81084c5f14e7b7b5476f6ff8193c29cde
photo-bro <josh.harmon@ED-001969.principia.local>
2013-11-26 10:34:41 -0600

    Adjusted size

--- Forms/Observation_Forms/Add_Group.Designer.cs
+++ Forms/Observation_Forms/Add_Group.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(374, 376);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Add_Group";
             this.Text = "Add_Group";

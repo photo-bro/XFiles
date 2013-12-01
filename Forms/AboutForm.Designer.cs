e477bf8aad8b5e052684d33626ab11658eb3dd42
photo-bro <josh.harmon@ED-001969.principia.local>
2013-12-01 16:11:51 -0600

    About form

---
+++ Forms/AboutForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace XFiles.Forms
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Splash = new System.Windows.Forms.PictureBox();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Splash)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Splash
+            // 
+            this.Splash.Image = global::XFiles.Properties.Resources.XFiles_Splash;
+            this.Splash.Location = new System.Drawing.Point(12, 12);
+            this.Splash.Name = "Splash";
+            this.Splash.Size = new System.Drawing.Size(360, 120);
+            this.Splash.TabIndex = 0;
+            this.Splash.TabStop = false;
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.BackColor = System.Drawing.Color.White;
+            this.tbDescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbDescription.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tbDescription.Location = new System.Drawing.Point(12, 138);
+            this.tbDescription.Multiline = true;
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.ReadOnly = true;
+            this.tbDescription.Size = new System.Drawing.Size(360, 116);
+            this.tbDescription.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Location = new System.Drawing.Point(297, 263);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(384, 298);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.tbDescription);
+            this.Controls.Add(this.Splash);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "AboutForm";
+            this.Text = "About XFiles";
+            ((System.ComponentModel.ISupportInitialize)(this.Splash)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Splash;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
---
+++ Forms/AboutForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XFiles.Forms
+{
+    public partial class AboutForm : Form
+    {
+        FileManager m_FM = FileManager.Instance;
+        public AboutForm()
+        {
+            InitializeComponent();
+            // Set description
+            tbDescription.Text = m_FM.ProgramName + " " + m_FM.ProgramVersion + "\r\n" +
+                "Josh Harmon & Kali McKee\r\n" + "CSCI 330 DBMS Fall 2013\r\n\r\n" +
+                "XFiles is a simple animal observation database and interface for cataloging various sightings of birds and creatures on the Principia College Campus";
+        } // AboutForm()
+        
+        /// <summary>
+        /// Close form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOK_Click(object sender, EventArgs e)
+        { this.Close(); }
+    } // AboutForm
+} // namespace XFiles.Forms

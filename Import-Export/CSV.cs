49b3fe3fe34fb4ce70651b40d9957a3456223dbb
photo-bro <photigragraphy@gmail.com>
2014-01-22 10:23:59 -0600

    Initial implementation of CSV import export. Referenced FileHelpers, need to credit still

---
+++ Import-Export/CSV.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data; // datatable
+using FileHelpers;      // CSV import/export
+
+
+namespace XFiles.Import_Export
+{
+    public class CSV
+    {
+        // Singleton stuff....
+
+
+
+
+        FileHelperEngine m_fhe = new FileHelperEngine(typeof(ObservationRecord));
+
+        public DataTable CSVToDataTable(string path, string name)
+        {
+            string fullpath = path + "//" + name + ".csv";
+            return m_fhe.ReadFileAsDT(fullpath);
+        }
+
+        public ObservationRecord[] CSVToObservations(string path, string name)
+        {
+            string fullpath = path + "//" + name + ".csv";
+            return m_fhe.ReadFile(fullpath) as ObservationRecord[];
+        }
+
+        public void ObservationsToCSV(ObservationRecord[] records, string path, string name)
+        {
+            string fullpath = path + "//" + name + ".csv";
+            m_fhe.WriteFile(fullpath, records);
+        }
+
+
+    }
+}
---
+++ Import-Export/ObservationRecord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FileHelpers;
+
+namespace XFiles.Import_Export
+{
+
+    /// <summary>
+    /// Class containging the attributes for the fields of a proper
+    /// observation CSV file.
+    /// </summary>
+    [DelimitedRecord(",")]
+    public class ObservationRecord
+    {
+
+
+    }
+
+
+}
